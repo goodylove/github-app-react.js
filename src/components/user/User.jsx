e2e0d9711f040c81e1ea3be4c23a3f69a09c140f
goodness <goodyc474@gmail.com>
2023-01-17 12:45:22 +0100

    add not found message

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -4,20 +4,27 @@ import "./User.css";
 
 const User = ({ user }) => {
   const { login, avatar_url, bio, following, followers } = user;
+  console.log(user.message);
   return (
-    <div className="user">
-      <img src={avatar_url} alt="" />
-      <span>{login}</span>
-      <span>{bio}</span>
-      <div className="follow-container">
-        <span>
-          <SlUserFollowing />
-          following
-          {followers}
-        </span>
-        <span> following{following}</span>
-      </div>
-    </div>
+    <>
+      {!user.message ? (
+        <div className="user">
+          <img src={avatar_url} alt="" />
+          <span>{login}</span>
+          <span>{bio}</span>
+          <div className="follow-container">
+            <span>
+              <SlUserFollowing />
+              following
+              {followers}
+            </span>
+            <span> following{following}</span>
+          </div>
+        </div>
+      ) : (
+        user.message
+      )}
+    </>
   );
 };
 
