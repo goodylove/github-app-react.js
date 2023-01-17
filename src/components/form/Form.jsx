0032878e2407a6b840e22e7da6345e365cd64c43
goodness <goodyc474@gmail.com>
2023-01-17 06:49:20 +0100

    added content in form components

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <>
+      <form action="">
+        <input type="text" placeholder="search any user of your choice" />
+        <button type="submit"></button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
